55cc4c9f43310da9deda420099fe93506aa8f384
Dylan Torres <91088586+DylanTorres56@users.noreply.github.com>
2024-10-16 15:21:55 -0400

    Observer In Progress
    The Observer pattern is currently in progress.

---
+++ Assets/Scripts/ScoreObservation.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreObservation : MonoBehaviour
+{
+    public TextMeshProUGUI scoreTracker;
+    int score;
+
+    private void OnEnable()
+    {
+        Goon.OnScoreUpdated += IncrementScore;
+    }
+
+    private void OnDisable()
+    {
+        Goon.OnScoreUpdated += IncrementScore;
+    }
+
+    public void IncrementScore() 
+    {
+        score++;
+        scoreTracker.text = $"Score: {score}";
+    }
+}
